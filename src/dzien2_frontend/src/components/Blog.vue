8681b8555bbe2a49f88eb2a2403ba157b859cd5b
uniiun <pg3252@proton.me>
2024-06-25 19:52:48 +0200

    dodanie obsługi bloga

---
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <button @click="pobierzWpisy">odswiez</button>
+        elo 
+        {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpis">dodaj</button>
+    </div>
+</template>
+
+<script>
+import { dzien2_backend } from 'declarations/dzien2_backend/index';
+    export default {
+        data() {
+            return {
+                wpisy:[],
+                nowyBlog: "" , 
+            }
+        },
+        methods: {
+            async pobierzWpisy(){
+                this.wpisy = await dzien2_backend.oczytaj_wpisy();
+            },
+            async dodajWpis(){
+               await dzien2_backend.dodaj_wpis(this.nowyBlog);
+            }
+        },
+    }
+</script>
